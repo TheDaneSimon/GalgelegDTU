e96a03e3d143cf37a99a2f2434d7895e268fa01c
Mads <mads@stege.eu>
2017-11-04 15:09:39 +0100

    Fixed main page and smaller cleaning

--- app/src/main/java/arcinc/galgeleg/MainActivity.java
+++ app/src/main/java/arcinc/galgeleg/MainActivity.java
@@ -1,15 +1,16 @@
 package arcinc.galgeleg;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     Button buttonHelp, buttonStart, buttonHighScore, buttonSettings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
